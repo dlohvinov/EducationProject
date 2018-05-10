a10693b599a9c25db6094551748cd4c8cf9e687d
gmike99 <gursky.michael1999@gmail.com>
2018-05-10 22:48:02 +0300

    fab removed

--- app/src/main/java/iot/lviv/ua/educationproject/MainActivity.java
+++ app/src/main/java/iot/lviv/ua/educationproject/MainActivity.java
@@ -75,12 +75,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-
-        //TODO: remove floating action button
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
